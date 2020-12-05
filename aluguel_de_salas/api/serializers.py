93d28cf699c7cf1dbf061605fad9204c09a95651
Felipe Almeida <felipeortalmeida@gmail.com>
2020-12-05 17:37:06 -0300

    Added resource URL in returned fields

--- aluguel_de_salas/api/serializers.py
+++ aluguel_de_salas/api/serializers.py
@@ -2,26 +2,26 @@ from rest_framework import serializers
 from aluguel_de_salas.api import models
 
 # Create your serializers here.
-class BaseSerializer():
-    fields = ['comments', 'created_at', 'updated_at']
-
 class CustomerSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = models.Customer
         fields = [
-            'name', 'cpf', 'birthday', 'phone', 'email',
-        ] + BaseSerializer.fields
+            'url', 'name', 'cpf', 'birthday', 'phone', 'email', 'comments',
+            'created_at', 'updated_at',
+        ]
 
 class RoomSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = models.Room
         fields = [
-            'name', 'size', 'capacity', 'description', 'price',
-        ] + BaseSerializer.fields
+            'url', 'name', 'size', 'capacity', 'description', 'price', 'comments',
+            'created_at', 'updated_at',
+        ]
 
 class RentSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = models.Rent
         fields = [
-            'customer', 'room', 'value', 'start', 'end',
-        ] + BaseSerializer.fields
+            'url', 'customer', 'room', 'value', 'start', 'end', 'comments',
+            'created_at', 'updated_at',
+        ]
